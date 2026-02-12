25d53e0e91d6aca43f0f34e65de64b99575ead5c
EmirataG <emirahmed9.7.5@gmail.com>
2026-02-12 13:51:51 -0500

    ui umprovement in log in page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,8 +5,23 @@ import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <div className="flex flex-col items-center">
+    <div className="min-h-screen flex items-center justify-center bg-black relative overflow-hidden">
+      {/* Scrolling score background */}
+      <div className="absolute inset-0 overflow-hidden">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/score.svg"
+          alt=""
+          className="w-full scroll-score-bg"
+          style={{
+            opacity: 0.15,
+            filter: "invert(1)",
+          }}
+        />
+      </div>
+
+      {/* Content */}
+      <div className="flex flex-col items-center relative z-10">
         {/* Logo */}
         <Image
           src="/manuscript_logo.png"
